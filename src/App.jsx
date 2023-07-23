269a772ecbe5a59ac49acef73ea7962c551dc213
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-23 22:58:30 +0200

    llamada en api

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+const RANDOM_FACT_URL = "https://catfact.ninja/facts";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { fact, setFact } = useState();
+
+  const getFact = () => {
+    fetch(RANDOM_FACT_URL)
+      .then((res) => res.json())
+      .then((response) => {
+        const { fact } = response;
+        setFact(fact);
+      });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>App de gatitos</h1>
+      <button onClick={getFact}>Buscar</button>
+      {fact && <p>{fact}</p>}
+    </div>
+  );
 }
 
-export default App
+export default App;
